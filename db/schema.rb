a3f1ce11e29f122db91bbb8f2b148018af08ff33
EC2 Default User <ec2-user@ip-172-31-5-128.us-east-2.compute.internal>
2018-06-01 01:53:46 +0000

    add profile fields to users closes #3

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180530020258) do
+ActiveRecord::Schema.define(version: 20180601015221) do
 
   create_table "games", force: :cascade do |t|
     t.string "board"
@@ -36,6 +36,9 @@ ActiveRecord::Schema.define(version: 20180530020258) do
     t.string "last_sign_in_ip"
     t.string "provider"
     t.string "uid"
+    t.string "last_name"
+    t.string "first_name"
+    t.integer "age"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
